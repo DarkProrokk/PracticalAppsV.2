2a8b63ed377acc7015575c6930752027b157d739
Сероштан Максим Андреевич <SeroshtanMA@medkhv.ru>
2024-02-06 10:06:42 +1000

    first commit

---
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Mvc.Models;
+using Packt.Shared;
+
+namespace Northwind.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly NorthwindContext db;
+
+    public HomeController(ILogger<HomeController> logger, NorthwindContext injectedContext)
+    {
+        _logger = logger;
+        db = injectedContext;
+    }
+
+    public IActionResult Index()
+    {
+        HomeIndexViewModel model = new(
+            VisitorCount: (new Random()).Next(1, 1001),
+            Categories: db.Categories.ToList(),
+            Products: db.Products.ToList()
+        );
+        return View(model); // передача модели представлению
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult ProductDetail(int? id)
+    {
+        if (!id.HasValue)
+        {
+            return BadRequest("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
+        }
+
+        Product? model = db.Products
+            .SingleOrDefault(p => p.ProductId == id);
+        if (model == null)
+        {
+            return NotFound($"ProductId {id} not found.");
+        }
+
+        return View(model);
+    }
+
+    public IActionResult ModelBinding()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult ModelBinding(Thing thing)
+    {
+        HomeModelBindingViewModel model = new(
+            thing,
+            !ModelState.IsValid,
+            ModelState.Values.SelectMany(state => state.Errors)
+                .Select(error => error.ErrorMessage)
+        );
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
